3f3beedaaf085da4092caacc888c1a3b9be8672b
Lê Hửu Phước <20130369@st.hcmuaf.edu.vn>
2024-04-16 14:57:59 +0700

    code login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </>
   );
