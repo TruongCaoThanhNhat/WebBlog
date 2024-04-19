f484c2239cfbc85d1d894b96f5972a2cecf86e56
ThanhNhat <truongcaothanhnhat@gmail.com>
2024-04-19 22:06:33 +0700

    add ui suggest

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import Suggest from "../components/suggest/Suggest";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <section className="main">
+        <Suggest />
+      </section>
       <Footer />
     </div>
   );
