7ec4cc9d5c03fb847764f70520e06887e83655a7
Lê Hửu Phước <20130369@st.hcmuaf.edu.vn>
2024-04-21 21:40:18 +0700

    code CateDetails  page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CateDetails from './pages/CateDetails';
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="catedetails" element={<CateDetails />} />
       </Routes>
     </>
   );
