e484e87a8fd8ebbba0deff070782ecdf201781e4
Lê Hửu Phước <20130369@st.hcmuaf.edu.vn>
2024-04-22 20:49:56 +0700

    code PostDetails page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CateDetails from './pages/CateDetails';
-
+import PostDetails from './pages/PostDetails';
 function App() {
   return (
     <>
@@ -13,6 +13,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="catedetails" element={<CateDetails />} />
+        <Route path="postdetails" element={<PostDetails />} />
       </Routes>
     </>
   );
