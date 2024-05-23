ef4a781ec5deb03b4420d3f06fd9a12e6bdc350c
ThanhNhat <truongcaothanhnhat@gmail.com>
2024-05-23 11:04:10 +0700

    setup server

---
+++ server/src/routes/initWebRoutes.js
@@ -0,0 +1,8 @@
+// import authRoutes from './authRoutes'
+// import user from './user'
+
+const webRoutes = (app) => {
+  // app.use('/api/v1/auth', authRoutes)
+  // app.use('/api/v1/user', user)
+}
+export default webRoutes
