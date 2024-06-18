597b5cd8d890b7caaa67dc7c59b4a919100b3926
ThanhNhat <truongcaothanhnhat@gmail.com>
2024-06-18 09:47:36 +0700

    update api create post, get all post

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -3,25 +3,83 @@ import { v2 as cloudinary } from "cloudinary";
 import { UserModel } from "../models/UserModel";
 import { PostModel } from "../models/PostModel";
 
-export const getAllPosts = async (req, res, next) => {
+export const getAllPosts = async (req, res) => {
   try {
-    const posts = await PostModel.find()
-      .sort({ createdAt: -1 })
+    const { sort, page = 1, limit = 20 } = req.query;
+
+    const endDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - 7);
+
+    let sortOptions = {};
+    let countOptions = {};
+    if (sort === "top") {
+      sortOptions = { point: -1 }; // Sắp xếp theo lượt like giảm dần
+    } else if (sort === "new") {
+      sortOptions = { createdAt: -1 }; // Sắp xếp theo ngày tạo mới nhất
+    } else if (sort === "view") {
+      sortOptions = { views: -1 }; // Sắp xếp theo lượt xem giảm dần
+    } else if (sort === "hot") {
+      sortOptions = { views: -1 }; // Sắp xếp theo lượt xem giảm dần
+      countOptions = { createdAt: { $gte: startDate, $lte: endDate } };
+    }
+
+    let query = PostModel.find()
+      .sort(sortOptions)
       .populate("author", "userName avatar displayName postSaved")
       .populate("category", "name slug")
-      .select("title content description createdAt slug category attachment ");
-    // populate('author') lấy toàn bộ thông tin của user có id match
-    // populate('author','userName') lấy username từ author
-    // .select('-slug') // trừ field nào không muốn lấy
-    res.status(200).json({
-      status: "OK",
-      data: {
-        posts,
-      },
-    });
+      .select(
+        "title content description createdAt slug category attachment vote views point comment_count"
+      );
+    // .select('-_id') // trừ field nào không muốn lấy
+
+    if (page && limit) {
+      const pageNumber = Number(page);
+      const limitNumber = Number(limit);
+      query = query.skip((pageNumber - 1) * limitNumber).limit(limitNumber);
+
+      let totalPosts = await PostModel.countDocuments(countOptions);
+      let totalPages = Math.ceil(totalPosts / limitNumber);
+
+      if (sort === "hot") {
+        query = query.find({ createdAt: { $gte: startDate, $lte: endDate } });
+      }
+      // Kiểm tra nếu không có bài đăng trong 3 ngày gần nhất
+      if (sort === "hot" && totalPosts === 0) {
+        query = PostModel.find();
+      }
+      const posts = await query;
+
+      res.status(200).json({
+        status: "OK",
+        data: {
+          posts,
+          pagination: {
+            totalPosts,
+            totalPages,
+            currentPage: pageNumber,
+            limit: limitNumber,
+          },
+        },
+      });
+    } else {
+      const posts = await query;
+      res.status(200).json({
+        status: "OK",
+        data: {
+          posts,
+          pagination: {
+            totalPosts,
+            totalPages,
+            currentPage: pageNumber,
+            limit: limitNumber,
+          },
+        },
+      });
+    }
   } catch (err) {
     res.status(500).json({
-      error: err,
+      error: err.message,
     });
   }
 };
@@ -49,63 +107,57 @@ export const getPostsByCategory = async (req, res, next) => {
 export const createPost = async (req, res, next) => {
   try {
     const { userId } = req.user;
-    if (req.body.title.length < 10) {
-      res.status(500).json({
+    console.log(req.body);
+    console.log(userId);
+    // Kiểm tra đầu vào
+    if (!req.body.title || req.body.title.length < 10) {
+      return res.status(400).json({
         err: "Tiêu đề bài viết không được để trống và phải nhiều hơn 10 kí tự",
       });
     }
-    // if (req.body.content.blocks.length === 0) {
-    //   res.status(500).json({
-    //     err: 'Có lỗi khi tạo bài viết',
-    //   })
-    // }
-    const att =
-      req.body.content && Array.isArray(req.body.content.blocks)
-        ? req.body.content.blocks.filter((url) => {
-            if (url.type === "image") {
-              return url.data.file;
-            }
-          })
-        : [];
-    const url = att.map((e) => {
-      return e.data.file.url;
-    });
-    if (url.length !== 0) {
-      const post = await PostModel.create({
-        ...req.body,
-        author: userId,
-        vote: userId,
-        attachment: url[0].toString(),
-      });
-      res.status(200).json({
-        status: "OK",
-        data: {
-          slug: post.slug,
-          id: post._id,
-          status: "Bài viết được tạo thành công",
-        },
-      });
-    } else if (url.length === 0) {
-      const post = await PostModel.create({
-        ...req.body,
-        author: userId,
-        vote: userId,
-      });
-      res.status(200).json({
-        status: "OK",
-        data: {
-          slug: post.slug,
-          id: post._id,
-          status: "Bài viết được tạo thành công",
-        },
+
+    if (!req.body.content || req.body.content.length === 0) {
+      return res.status(400).json({
+        err: "Nội dung bài viết không được để trống",
       });
     }
+
+    // Xử lý attachment
+    const att =
+      req.body.content && Array.isArray(req.body.content)
+        ? req.body.content.filter(
+            (url) => url.type === "image" && url.data.file
+          )
+        : [];
+
+    const url = att.map((e) => e.data.file.url);
+
+    // Tạo bài viết
+    const postData = {
+      ...req.body,
+      author: userId,
+      vote: userId,
+      attachment: url.length ? url[0].toString() : undefined,
+    };
+
+    const post = await PostModel.create(postData);
+
+    res.status(201).json({
+      status: "OK",
+      data: {
+        slug: post.slug,
+        id: post._id,
+        status: "Bài viết được tạo thành công",
+      },
+    });
   } catch (err) {
+    console.error("Error creating post:", err); // Log chi tiết lỗi
     res.status(500).json({
       err: "Có lỗi khi tạo bài viết",
     });
   }
 };
+
 export const updatePost = async (req, res, next) => {
   try {
     const { postId } = req.params;
